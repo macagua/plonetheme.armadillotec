570d7b2f963382291f7adaffce529aff7ccc314d
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-09-20 13:14:21 -0430

    Renamed name files and id, class selector from Spanish to English

--- plonetheme/armadillotec/static/scripts/theme.js
+++ plonetheme/armadillotec/static/scripts/theme.js
@@ -1,3 +1,20 @@
+/* Google Web Fonts 
+
+  WebFontConfig = {
+    google: { families: [ 'Homenaje::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+  */
+
+
 /* Dreamweaver common funcions */
 
 function MM_preloadImages() { //v3.0
