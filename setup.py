17ab3db1da9f5d9643fef56c8cbea68968a853cf
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-17 18:37:30 -0400

    Added more strings classifiers items for this package

--- setup.py
+++ setup.py
@@ -3,26 +3,34 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.1.1'
 
 setup(name='plonetheme.armadillotec',
       version=version,
-      description='An installable Diazo theme for Plone 4.1',
+      description='ArmadilloTec, is an installable Diazo theme for Plone 4',
       long_description=open('README.rst', 'rb').read()+'\n'+
                        open(os.path.join("docs", "INSTALL.txt")).read()+'\n'+
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: Theme',
         'Framework :: Zope2',
         'Framework :: Zope3',
-        'Programming Language :: Python',
         'Intended Audience :: Developers',
-        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
       keywords='web zope plone diazo theme armadillotec cms',
       author='Leonardo J. Caballero G.',
